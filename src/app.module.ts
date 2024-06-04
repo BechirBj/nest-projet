72703b98aea98da7e1d5c88dcf339b5487dc6ddc
Bechir Ben Jazia <bechir.benjazia8@gmail.com>
2024-06-04 12:07:39 +0100

    App module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,9 @@
 import {Module} from "@nestjs/common";
-import {UserModule} from "./user/users.module";
+import {UsersModule} from "./users/users.module";
 
 
 @Module({ 
-    imports: [UserModule],
+    imports: [UsersModule],
 })
 export class AppModule{
 
