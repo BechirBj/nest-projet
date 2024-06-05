159c4828f4837f30ddbfd212909558a4ed6d6085
Bechir Ben Jazia <bechir.benjazia8@gmail.com>
2024-06-05 23:07:31 +0100

    Find, Find One ,Remove ,Update

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,4 @@
-import { Controller,Delete,Get,Patch,Post, Req ,Param, Body, HttpCode} from "@nestjs/common";
+import { Controller,Delete,Get,Patch,Post, Req ,Param, Body, HttpCode, HttpStatus} from "@nestjs/common";
 import { CreateUserDto } from "./dtos/CreateUser.tdo";
 import { UpdateUserDto } from "./dtos/UpdateUser.tdo";
 import { UserEntity } from "./User.entity";
@@ -8,7 +8,7 @@ import {v4 as uuid} from "uuid"
 export class UsersController{
 
 
-    private readonly users: UserEntity[] = [];
+    private  users: UserEntity[] = [];
 
 
 
@@ -20,7 +20,7 @@ export class UsersController{
     @Get(":id") // returs the user that was passed in the GET request (localhost:3000/users/id)
     findOne(@Param("id") id: string): UserEntity {
         // Mapping 3al users 7ata len yal9a l id == user.id
-        return  this.users.find((user) => user.id == id);
+        return  this.users.find((user) => user.id === id);
     }
 
 
@@ -40,18 +40,30 @@ export class UsersController{
 
 
 
-    @Patch(":username")       
-    update(@Param("username") username: string, @Body() updateUserDto : UpdateUserDto ){
-        
-        return updateUserDto ;
+    @Patch(":id")       
+    update(@Param("id") id: string, @Body() updateUserDto : UpdateUserDto ){
+        // 1) find the user that has the passed id 
+        const index = this.users.findIndex((user : UserEntity) => user.id === id); 
+        // 2) update the user
+        const Updateduser = this.users[index] = {
+           ...this.users[index],
+           ...updateUserDto,
+           // Ovveride the user with the data that was passed on @body() parametre
+        }
+        return Updateduser ;
+        // direct methode : 
+        // return this.users[index] ; 
+
 
     }
+    @Delete(":id")   
+    @HttpCode(HttpStatus.NO_CONTENT)  // You can change the status code here  to indicate the result of a request
+    remove(@Param("id", ) id: string)  {
+        // 1) find the user that has the passed id 
+        this.users = this.users.filter((user : UserEntity) => user.id != id  );
+        // 2) remove the user
 
-    @Delete(":username")   
-    @HttpCode(204)  // You can change the status code here  to indicate the result of a request
-    remove(@Param("username", ) username: string) {
-    }
-    
+    }   
     
     /* 
     @Post()
